7f46517af2be475c7959dab5a912fff976dcc362
Thorax <thorax@me.com>
2017-04-11 13:31:27 +0300

    add localizing utilities

--- Teambrella/Helpers/Extensions/String.swift
+++ Teambrella/Helpers/Extensions/String.swift
@@ -9,4 +9,17 @@ extension String {
     var base58Data: Data {
         return BTCDataFromBase58(self) as Data
     }
+    
+    var localized: String {
+        guard let range = self.range(of: ".") else  { return self }
+        
+        return NSLocalizedString(self,
+                                 tableName: self.substring(to: range.lowerBound),
+                                 comment: self)
+    }
+    
+    func localized(_ arguments: CVarArg...) -> String {
+        let template = localized
+        return String(format: template, arguments: arguments)
+    }
 }
