459962ef1270c1ea8e0e5c3bc3ed5a15a387c5bf
Thorax <thorax@me.com>
2017-05-25 18:59:31 +0300

    add Team controller prototypes

---
+++ Teambrella/Scenes/Team/Claims/ClaimsVC.swift
@@ -0,0 +1,38 @@
+//
+//  ClaimsVC.swift
+//  Teambrella
+//
+//  Created by Yaroslav Pasternak on 25.05.17.
+//  Copyright © 2017 Yaroslav Pasternak. All rights reserved.
+//
+
+import UIKit
+import XLPagerTabStrip
+
+class ClaimsVC: UIViewController, IndicatorInfoProvider {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
+        return IndicatorInfo(title: "Claims")
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Teambrella/Scenes/Team/TeamVC.swift
@@ -0,0 +1,54 @@
+//
+//  TeamVC.swift
+//  Teambrella
+//
+//  Created by Yaroslav Pasternak on 25.05.17.
+//  Copyright © 2017 Yaroslav Pasternak. All rights reserved.
+//
+
+import UIKit
+import XLPagerTabStrip
+
+class TeamVC: ButtonBarPagerTabStripViewController {
+
+    override func viewDidLoad() {
+        setup()
+        super.viewDidLoad()
+
+        navigationController?.navigationBar.barTintColor = .clear
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func setup() {
+        settings.style.buttonBarBackgroundColor = .clear
+        settings.style.buttonBarItemBackgroundColor = .clear
+        settings.style.selectedBarBackgroundColor = .teambrellaLightBlue
+        settings.style.buttonBarItemFont = .boldSystemFont(ofSize: 14)
+        settings.style.selectedBarHeight = 4.0
+        settings.style.buttonBarMinimumLineSpacing = 0
+        settings.style.buttonBarItemTitleColor = .whiteHalfTransparent
+        settings.style.buttonBarItemsShouldFillAvailiableWidth = true
+        settings.style.buttonBarLeftContentInset = 0
+        settings.style.buttonBarRightContentInset = 0
+        changeCurrentIndexProgressive = { oldCell, newCell, progressPercentage, changeCurrentIndex, animated -> Void in
+            guard changeCurrentIndex == true else { return }
+            
+            oldCell?.label.textColor = .whiteHalfTransparent
+            newCell?.label.textColor = .white
+        }
+    }
+    
+    override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
+        let feed = UIStoryboard(name: "Team", bundle: nil).instantiateViewController(withIdentifier: "FeedVC")
+        let members = UIStoryboard(name: "Team", bundle: nil).instantiateViewController(withIdentifier: "MembersVC")
+        let claims = UIStoryboard(name: "Team", bundle: nil).instantiateViewController(withIdentifier: "ClaimsVC")
+        let rules = UIStoryboard(name: "Team", bundle: nil).instantiateViewController(withIdentifier: "RulesVC")
+        return [feed, members, claims, rules]
+    }
+}
