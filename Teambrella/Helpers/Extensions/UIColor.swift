b59a4fc03a8474bc486b48679fb566158d44b838
Thorax <thorax@me.com>
2017-05-26 18:48:52 +0300

    apply design to home screen 2/3

--- Teambrella/Helpers/Extensions/UIColor.swift
+++ Teambrella/Helpers/Extensions/UIColor.swift
@@ -9,9 +9,13 @@
 import UIKit
 
 extension UIColor {
+    // MARK: Blue
     class var teambrellaBlue: UIColor { return          #colorLiteral(red: 0.1333333333, green: 0.2, blue: 0.5176470588, alpha: 1) }
     class var teambrellaLightBlue: UIColor { return     #colorLiteral(red: 0.3058823529, green: 0.768627451, blue: 0.9490196078, alpha: 1) }
     class var frenchBlue: UIColor { return              #colorLiteral(red: 0.2588235294, green: 0.3058823529, blue: 0.7098039216, alpha: 1) }
     class var warmBlue: UIColor { return                #colorLiteral(red: 0.3215686275, green: 0.368627451, blue: 0.8666666667, alpha: 1) }
+    class var darkSkyBlue: UIColor { return             #colorLiteral(red: 0.2078431373, green: 0.6705882353, blue: 0.8470588235, alpha: 1) }
+    
+    // MARK: White
     class var whiteHalfTransparent: UIColor { return    #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 0.5) }
 }
---
+++ Teambrella/View/BrickView.swift
@@ -0,0 +1,136 @@
+//
+//  BrickView.swift
+//  Teambrella
+//
+//  Created by Yaroslav Pasternak on 26.05.17.
+//  Copyright © 2017 Yaroslav Pasternak. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class BrickView: UIView {
+    struct Constant {
+        static let standardOffset: CGFloat = 16.0
+        static let smallFontSize: CGFloat = 9.0
+        static let titleFontSize: CGFloat = 10.0
+        static let largeFontSize: CGFloat = 24.0
+    }
+    
+    enum BrickViewCurrencyType {
+        case normal
+        case upper
+        case lower
+    }
+    
+    @IBInspectable
+    var icon: UIImage?
+    @IBInspectable
+    var title: String = ""
+    @IBInspectable
+    var amount: String = "0"
+    @IBInspectable
+    var currency: String = "USD"
+   
+    @IBInspectable
+    var badge: String?
+    
+    var currencyType: BrickViewCurrencyType = .normal
+    var amountTextColor: UIColor = .black
+    var titleTextColor: UIColor = .whiteHalfTransparent
+    var currencyTextColor: UIColor = .darkSkyBlue
+    var badgeBackgroundColor: UIColor = .darkSkyBlue
+    var badgeTextColor: UIColor = .white
+    
+    lazy private var iconView: UIImageView = {
+        let view = UIImageView()
+        self.addSubview(view)
+        return view
+    }()
+    
+    lazy private var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: Constant.titleFontSize)
+        label.textColor = self.titleTextColor
+        self.addSubview(label)
+        return label
+    }()
+    
+    lazy private var amountLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: Constant.largeFontSize)
+        label.textColor = self.amountTextColor
+        self.addSubview(label)
+        return label
+    }()
+    
+    lazy private var currencyLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: Constant.largeFontSize)
+        label.textColor = self.currencyTextColor
+        self.addSubview(label)
+        return label
+    }()
+    
+    lazy private var badgeLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: Constant.smallFontSize)
+        label.textColor = self.badgeTextColor
+        label.backgroundColor = self.badgeBackgroundColor
+        self.addSubview(label)
+        return label
+    }()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        switch currencyType {
+        case .normal:
+            currencyLabel.font = amountLabel.font
+        default:
+            currencyLabel.font = UIFont(name: amountLabel.font.fontName, size: Constant.smallFontSize)
+        }
+        
+        if let icon = icon {
+            iconView.image = icon
+            iconView.center = CGPoint(x: iconView.frame.width / 2, y: bounds.midY)
+            let x = iconView.frame.maxX + Constant.standardOffset
+            amountLabel.frame.origin = CGPoint(x: x, y: iconView.frame.maxY - amountLabel.frame.height)
+            titleLabel.frame.origin = CGPoint(x: x, y: iconView.frame.minY)
+        } else if let badge = badge {
+            titleLabel.center = CGPoint(x: bounds.midX, y: bounds.midY - titleLabel.frame.height / 2)
+            
+            let wdt = amountLabel.frame.width
+                + currencyLabel.frame.width
+                + Constant.standardOffset
+                + badgeLabel.frame.width
+            
+            let x = (bounds.width - wdt) / 2
+            amountLabel.frame.origin = CGPoint(x: x, y: titleLabel.frame.maxY)
+            currencyLabel.frame.origin = CGPoint(x: amountLabel.frame.maxX, y: 0)
+            badgeLabel.frame.origin = CGPoint(x: currencyLabel.frame.maxX + Constant.standardOffset,
+                                              y: amountLabel.frame.midY)
+            badgeLabel.text = badge
+        } else {
+            titleLabel.center = CGPoint(x: bounds.midX, y: bounds.midY - titleLabel.frame.height / 2)
+            
+            let wdt = amountLabel.frame.width
+                + currencyLabel.frame.width
+            
+            let x = (bounds.width - wdt) / 2
+            amountLabel.frame.origin = CGPoint(x: x, y: titleLabel.frame.maxY)
+            currencyLabel.frame.origin = CGPoint(x: amountLabel.frame.maxX, y: 0)
+        }
+        
+        let x = amountLabel.frame.maxX
+        switch currencyType {
+        case .normal:
+            currencyLabel.frame.origin = CGPoint(x: x, y: amountLabel.frame.minY)
+        case .lower:
+            currencyLabel.frame.origin = CGPoint(x: x, y: amountLabel.frame.maxY - currencyLabel.frame.height)
+        case .upper:
+            currencyLabel.frame.origin = CGPoint(x: x, y: amountLabel.frame.minY)
+        }
+    }
+
+}
