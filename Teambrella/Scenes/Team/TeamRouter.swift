0fd468db9b988a12e09bfd55f398263eebf801d4
Thorax <thorax@me.com>
2017-06-06 17:13:20 +0300

    add Chattoo

--- Teambrella/Scenes/Team/TeamRouter.swift
+++ Teambrella/Scenes/Team/TeamRouter.swift
@@ -9,5 +9,15 @@
 import Foundation
 
 final class TeamRouter {
+    func presentChat(teammate: TeammateLike) {
+        guard let topic = teammate.extended?.topic else { return }
+        
+        presentChat(topic: topic)
+    }
     
+    func presentChat(topic: Topic) {
+        guard let vc = ChatVC.instantiate() as? ChatVC else { fatalError("Error instantiating") }
+        vc.createDataSource(topic: topic)
+        service.router.push(vc: vc)
+    }
 }
