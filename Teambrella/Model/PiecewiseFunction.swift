8b3c35377b0f0fe779f389e810ee8e2ca7870368
Thorax <thorax@me.com>
2017-06-21 20:50:31 +0300

    add piecewise linear function

--- Teambrella/Model/PiecewiseFunction.swift
+++ Teambrella/Model/PiecewiseFunction.swift
@@ -8,6 +8,9 @@
 
 import Foundation
 
+/*
+ Кусочно линейная функция
+ */
 struct PiecewiseFunction {
     struct Item {
         let x: Double
@@ -18,6 +21,10 @@ struct PiecewiseFunction {
     var minValue: Double { return items.reduce(Double.greatestFiniteMagnitude) { $0 > $1.value ? $1.value : $0 } }
     var maxValue: Double { return items.reduce(-Double.greatestFiniteMagnitude) { $0 < $1.value ? $1.value : $0 } }
     
+    init(_ args:(Double, Double)...) {
+        items = args.map { Item(x: $0.0, value: $0.1) }
+    }
+    
     mutating func addPoint(x: Double, value: Double) {
         let item = Item(x: x, value: value)
         items.append(item)
@@ -25,9 +32,17 @@ struct PiecewiseFunction {
     }
     
     func value(at point: Double) -> Double? {
-//        var lesser: Item?
-//        var larger: Item?
-        return nil
+        let lesser: Item? = items.filter { $0.x < point }.last
+        let larger: Item? = items.filter { $0.x > point }.first
+        guard let less = lesser, let more = larger else { return nil }
+        
+        // (y-y1)/(y2-y1)=(x-x1)/(x2-x1)
+        let y1 = less.value
+        let y2 = more.value
+        let x1 = less.x
+        let x2 = more.x
+        let x = point
+        return y1 + (x - x1) / (x2 - x1) * (y2 - y1)
     }
     
 }
