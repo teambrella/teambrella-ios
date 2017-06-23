52970c771ea992d5520ec13bd97a7a6701a542fe
Ekaterina Ryzhova <rubicante@icloud.com>
2017-06-23 20:13:34 +0300

    add WalletDetailsVC, WalletDetailsVCScene and it’s components
    Signed-off-by: Ekaterina Ryzhova <rubicante@icloud.com>

---
+++ Teambrella/Scenes/Me/Wallet/WalletDetailsVC.swift
@@ -0,0 +1,41 @@
+//
+//  WalletDetailsVC.swift
+//  Teambrella
+//
+//  Created by Екатерина Рыжова on 23.06.17.
+//  Copyright © 2017 Yaroslav Pasternak. All rights reserved.
+//
+
+import UIKit
+
+class WalletDetailsVC: UIViewController, Routable {
+
+    static let storyboardName = "Me"
+    @IBOutlet var qrCodeImageView: UIImageView!
+    @IBOutlet var fundButton: UIButton!
+    @IBOutlet var bitcoinAddressLabel: Label!
+    @IBOutlet var container: UICollectionReusableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        CellDecorator.shadow(for: container)
+        CellDecorator.roundedEdges(for: container)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
