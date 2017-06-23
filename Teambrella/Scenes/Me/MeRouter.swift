5b3e2bb58624b23e8feab913fb8cc8f69d4113e0
Thorax <thorax@me.com>
2017-06-23 18:28:33 +0300

    add MeRouter

---
+++ Teambrella/Scenes/Me/MeRouter.swift
@@ -0,0 +1,17 @@
+//
+//  MeRouter.swift
+//  Teambrella
+//
+//  Created by Yaroslav Pasternak on 23.06.17.
+//  Copyright © 2017 Yaroslav Pasternak. All rights reserved.
+//
+
+import Foundation
+
+final class MeRouter {
+    func presentWalletDetails() {
+//        guard let vc = WalletDetailsVC.instantiate() as? WalletDetailsVC else { fatalError("Error instantiating") }
+//        service.router.push(vc: vc)
+    }
+    
+}
