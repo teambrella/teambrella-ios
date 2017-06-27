70018d432a13609feff8b70e45d144290258602d
Ekaterina Ryzhova <rubicante@icloud.com>
2017-06-27 16:13:56 +0300

    add connections to MeCell elements, remove excess icons from Assets
    Signed-off-by: Ekaterina Ryzhova <rubicante@icloud.com>

--- Teambrella/View/Cells/MeCell.swift
+++ Teambrella/View/Cells/MeCell.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class MeCell: UICollectionViewCell, XIBInitableCell {
-
+    @IBOutlet var radarView: RadarView!
+    @IBOutlet var avatar: RoundImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var infoLabel: UILabel!
+    @IBOutlet var stackView: UIStackView!
+    @IBOutlet var facebookButton: UIButton!
+    @IBOutlet var twitterButton: UIButton!
+    @IBOutlet var emailButton: UIButton!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
