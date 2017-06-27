d2948fdc5bc4aba54d8d4737fe4440bf8dfe4afa
Ekaterina Ryzhova <rubicante@icloud.com>
2017-06-27 14:37:46 +0300

    add MeCell and it’s XIB, and social-images to Assets
    Signed-off-by: Ekaterina Ryzhova <rubicante@icloud.com>

---
+++ Teambrella/View/Cells/MeCell.swift
@@ -0,0 +1,18 @@
+//
+//  MeCell.swift
+//  Teambrella
+//
+//  Created by Екатерина Рыжова on 27.06.17.
+//  Copyright © 2017 Yaroslav Pasternak. All rights reserved.
+//
+
+import UIKit
+
+class MeCell: UICollectionViewCell, XIBInitableCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
