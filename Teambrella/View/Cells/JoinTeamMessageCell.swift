0af1f96a4662bcc3e367d8ccc54e4f5ba019179c
Ekaterina Ryzhova <rubicante@icloud.com>
2017-07-01 18:41:02 +0300

    fifth cell done

---
+++ Teambrella/View/Cells/JoinTeamMessageCell.swift
@@ -0,0 +1,22 @@
+//
+//  JoinTeamMessageCell.swift
+//  Teambrella
+//
+//  Created by Екатерина Рыжова on 01.07.17.
+//  Copyright © 2017 Yaroslav Pasternak. All rights reserved.
+//
+
+import UIKit
+
+class JoinTeamMessageCell: UICollectionViewCell, XIBInitableCell {
+    @IBOutlet var headerLabel: UILabel!
+    @IBOutlet var message: LabeledTextField!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        headerLabel.text = "Team".uppercased()
+        message.headerLabel.text = "Message to teammates".uppercased()
+        message.textField.placeholder = "Introduce yourself to your future team mates by telling them a bit more about yourself. Note that this will help them approve of decline your application"
+    }
+
+}
