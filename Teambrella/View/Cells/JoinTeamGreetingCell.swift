ddede7e9e1e7bb1fa9c9e43990ccfce54b1aa820
Ekaterina Ryzhova <rubicante@icloud.com>
2017-07-01 15:45:36 +0300

    add JoinTeamGreetingCell and Xib

---
+++ Teambrella/View/Cells/JoinTeamGreetingCell.swift
@@ -0,0 +1,18 @@
+//
+//  JoinTeamGreetingCell.swift
+//  Teambrella
+//
+//  Created by Екатерина Рыжова on 01.07.17.
+//  Copyright © 2017 Yaroslav Pasternak. All rights reserved.
+//
+
+import UIKit
+
+class JoinTeamGreetingCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
