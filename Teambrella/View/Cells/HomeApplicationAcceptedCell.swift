18a6bffd024d1b92a3bf092492902cb4b18b908a
Ekaterina Ryzhova <rubicante@icloud.com>
2017-07-07 18:23:51 +0300

    add ApplicationAcceptedCell with localizations

---
+++ Teambrella/View/Cells/HomeApplicationAcceptedCell.swift
@@ -0,0 +1,18 @@
+//
+//  HomeApplicationAcceptedCell.swift
+//  Teambrella
+//
+//  Created by Екатерина Рыжова on 07.07.17.
+//  Copyright © 2017 Yaroslav Pasternak. All rights reserved.
+//
+
+import UIKit
+
+class HomeApplicationAcceptedCell: UICollectionViewCell {
+    @IBOutlet var backView: RadarView!
+    @IBOutlet var avatar: RoundImageView!
+    @IBOutlet var yummigum: UIImageView!
+    @IBOutlet var headerLabel: UILabel!
+    @IBOutlet var centerLabel: UILabel!
+    @IBOutlet var button: BorderedButton!
+}
