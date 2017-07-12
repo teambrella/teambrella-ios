ba26968a9b422f1e08d3e159df95ce975485e873
Thorax <thorax@me.com>
2017-07-12 20:31:12 +0300

    >

--- Teambrella/View/LabeledRoundImageView.swift
+++ Teambrella/View/LabeledRoundImageView.swift
@@ -13,7 +13,7 @@ class LabeledRoundImageView: UIView {
     @IBInspectable
     var textColor: UIColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
     @IBInspectable
-    var labelBackgroundColor: UIColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
+    var labelBackgroundColor: UIColor = .blueWithAHintOfPurple
     @IBInspectable
     var riskLabelText: String = "" {
         didSet {
