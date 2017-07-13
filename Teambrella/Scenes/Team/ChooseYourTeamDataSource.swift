4fe37b8d08e1afba6dc74cc861e931a55ba17e7e
Ekaterina Ryzhova <rubicante@icloud.com>
2017-07-13 18:29:05 +0300

    >

--- Teambrella/Scenes/Team/ChooseYourTeamDataSource.swift
+++ Teambrella/Scenes/Team/ChooseYourTeamDataSource.swift
@@ -12,10 +12,9 @@ struct ChooseYourTeamDataSource {
     var count: Int { return models.count }
     var models: [ChooseYourTeamCellModel] = []
     
-    mutating func createFakeModels() {
-        models = [ChooseYourTeamCellModel(),
-                  ChooseYourTeamCellModel(topText: "Proxy.SortVC.Cell.Rating".localized,
-                                bottomText: "Proxy.SortVC.Cell.Rating.LowHigh".localized)]
+    mutating func createModels() {
+        service.session.currentTeam
+        models = []
     }
     
     subscript(indexPath: IndexPath) -> ChooseYourTeamCellModel {
@@ -23,3 +22,32 @@ struct ChooseYourTeamDataSource {
     }
     
 }
+
+/*
+ struct TeamEntity {
+ private var json: JSON
+ 
+ init(json: JSON) {
+ self.json = json
+ }
+ 
+ var teamID: Int { return json["TeamId"].intValue }
+ var teamType: Int { return json["TeamType"].intValue }
+ var teamName: String { return json["TeamName"].stringValue }
+ var objectName: String? { return json["ObjectName"].stringValue }
+ var objectCoverage: Double? { return json["ObjectCoverage"].doubleValue }
+ var unreadCount: Int? { return json["UnreadCount"].intValue }
+ var teamCoverage: Double? { return json["TeamCoverage"].doubleValue }
+ 
+ var isInvitation: Bool { return teamCoverage != nil }
+ 
+ static func team(with json: JSON) -> TeamEntity {
+ return TeamEntity(json: json)
+ }
+ 
+ static func teams(with json: JSON) -> [TeamEntity] {
+ return json.arrayValue.map { self.team(with: $0) }
+ }
+ }
+
+ */
