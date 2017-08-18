6c773f389438ef08be8ddf9400ff0b1a3d7c678d
Thorax <thorax@me.com>
2017-08-18 12:19:31 +0300

    add Report files

---
+++ Teambrella/Scenes/Me/Report/ReportCellModels.swift
@@ -0,0 +1,9 @@
+//
+//  ReportCellModels.swift
+//  Teambrella
+//
+//  Created by Yaroslav Pasternak on 18.08.17.
+//  Copyright © 2017 Yaroslav Pasternak. All rights reserved.
+//
+
+import Foundation
