cba5732996432944aea4d73ef8f9f4d564e4ff82
Thorax <thorax@me.com>
2017-08-21 18:22:07 +0300

    add photoPreview files

---
+++ Teambrella/Controller/PhotosPreviewVC/PhotoPreviewVC.swift
@@ -0,0 +1,23 @@
+//
+//  PhotoPreviewVC.swift
+//  Teambrella
+//
+//  Created by Yaroslav Pasternak on 21.08.17.
+//  Copyright © 2017 Yaroslav Pasternak. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class PhotoPreviewVC: UICollectionViewController {
+    var photos: [String] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        registerCells()
+    }
+    
+    private func registerCells() {
+        
+    }
+}
---
+++ Teambrella/View/Cells/PhotoPreviewCell.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoPreviewCell.swift
+//  Teambrella
+//
+//  Created by Yaroslav Pasternak on 21.08.17.
+//  Copyright © 2017 Yaroslav Pasternak. All rights reserved.
+//
+
+import UIKit
+
+class PhotoPreviewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
