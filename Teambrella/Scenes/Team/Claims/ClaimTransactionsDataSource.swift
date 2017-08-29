37ed798b57d1fe86565c0294a776f370edde8e64
Ekaterina Ryzhova <rubicante@icloud.com>
2017-08-29 13:30:21 +0300

    add ClaimTransactionsDataSource

---
+++ Teambrella/Scenes/Team/Claims/ClaimTransactionsDataSource.swift
@@ -0,0 +1,13 @@
+//
+//  ClaimTransactionsDataSource.swift
+//  Teambrella
+//
+//  Created by Екатерина Рыжова on 29.08.17.
+//  Copyright © 2017 Yaroslav Pasternak. All rights reserved.
+//
+
+import Foundation
+
+class ClaimTransactionsDataSource {
+    
+}
