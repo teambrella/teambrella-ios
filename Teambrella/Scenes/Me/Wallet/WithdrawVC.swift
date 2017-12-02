3dbb63426f7f93250f420d6ba3dc98b814595809
Ekaterina Ryzhova <rubicante@icloud.com>
2017-12-02 16:11:41 +0300

    add WithdrawCellModels and WithdrawCellBuilder

--- Teambrella/Scenes/Me/Wallet/WithdrawVC.swift
+++ Teambrella/Scenes/Me/Wallet/WithdrawVC.swift
@@ -15,21 +15,53 @@
  */
 
 import UIKit
+import PKHUD
 
 class WithdrawVC: UIViewController, CodeCaptureDelegate {
 
     @IBOutlet var backView: UIView!
     @IBOutlet var collectionView: UICollectionView!
     
+    let dataSource = MembersDatasource(orderByRisk: false)
+    fileprivate var previousScrollOffset: CGFloat = 0
+    
+    var isFirstLoading = true
+    
+    // MARK: Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        HUD.show(.progress, onView: view)
+        //ask this
+        collectionView.register(WithdrawDetailsCell.nib, forCellWithReuseIdentifier: WithdrawDetailsCell.cellID)
 
-        // Do any additional setup after loading the view.
+        dataSource.onUpdate = { [weak self] in
+            HUD.hide()
+            self?.collectionView.reloadData()
+        }
+        
+        dataSource.onError = { [weak self] error in
+            HUD.hide()
+            guard let error = error as? TeambrellaError else { return }
+            
+            let controller = UIAlertController(title: "Error", message: error.description, preferredStyle: .alert)
+            let cancel = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            controller.addAction(cancel)
+            self?.present(controller, animated: true, completion: nil)
+        }
+        
+        dataSource.loadData()
+        title = "Me.Wallet.Withdraw".localized
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        guard isFirstLoading == false else {
+            isFirstLoading = false
+            return
+        }
+        
+        dataSource.updateSilently()
     }
     
     func codeCapture(controller: CodeCaptureVC, didCapture: String, type: QRCodeType) {
@@ -40,14 +72,103 @@ class WithdrawVC: UIViewController, CodeCaptureDelegate {
         
     }
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+// MARK: UICollectionViewDataSource
+extension WithdrawVC: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return dataSource.sections
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return dataSource.itemsInSection(section: section)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: UICollectionViewCell!
+        /////fixIt
+        switch dataSource.type(indexPath: indexPath) {
+        case .new:
+            cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CandidateCell",
+                                                      for: indexPath)
+        case .teammate:
+            cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TeammateCell",
+                                                      for: indexPath)
+        }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        viewForSupplementaryElementOfKind kind: String,
+                        at indexPath: IndexPath) -> UICollectionReusableView {
+        let view = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionElementKindSectionHeader,
+                                                                   withReuseIdentifier: "WithdrawHeader",
+                                                                   for: indexPath)
+        return view
+    }
+    
+}
 
+// MARK: UICollectionViewDelegate
+extension WithdrawVC: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView,
+                        willDisplay cell: UICollectionViewCell,
+                        forItemAt indexPath: IndexPath) {
+        let transaction = dataSource[indexPath]
+        /////fixIt
+        MembersCellBuilder.populate(cell: cell, with: transaction)
+        
+        let maxRow = dataSource.itemsInSection(section: indexPath.section)
+        if let cell = cell as? WithdrawCell {
+            cell.separator.isHidden = indexPath.row == maxRow - 1
+            ViewDecorator.decorateCollectionView(cell: cell,
+                                                 isFirst: indexPath.row == 0,
+                                                 isLast: indexPath.row == maxRow - 1)
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        willDisplaySupplementaryView view: UICollectionReusableView,
+                        forElementKind elementKind: String,
+                        at indexPath: IndexPath) {
+        if let view = view as? WithdrawHeader {
+            view.leadingLabel.text = dataSource.headerTitle(indexPath: indexPath)
+            view.trailingLabel.text = dataSource.headerSubtitle(indexPath: indexPath)
+        }
+    }
+
+}
+
+// MARK: UICollectionViewDelegateFlowLayout
+extension WithdrawVC: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.bounds.width, height: 72)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: collectionView.bounds.width, height: 56)
+    }
+}
+
+// MARK: UIScrollViewDelegate
+extension WithdrawVC: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        /*
+         let currentOffset = scrollView.contentOffset.y
+         let velocity = currentOffset - previousScrollOffset
+         previousScrollOffset = currentOffset
+         
+         if velocity > 10 {
+         showSearchBar(show: false, animated: true)
+         }
+         if velocity < -10 {
+         showSearchBar(show: true, animated: true)
+         }
+         */
+    }
 }
