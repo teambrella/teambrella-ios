84d5b9b25dfed5bf0e2d4708d4e718236383a6a8
Thorax <thorax@me.com>
2017-12-02 15:48:46 +0300

    ethereum address 1/2 done

---
+++ Teambrella/Model/Entities/EthereumAddress.swift
@@ -0,0 +1,54 @@
+//
+/* Copyright(C) 2017 Teambrella, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License(version 3) as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see<http://www.gnu.org/licenses/>.
+ */
+
+import Foundation
+import SwiftKeccak
+
+struct EthereumAddress {
+    let string: String
+    let hasChecksum: Bool
+    
+    init?(string: String) {
+        var string = string
+        if string.hasPrefix("0x") {
+            let index = string.index(string.startIndex, offsetBy: 2)
+            string = String(string[index...])
+        }
+        let pattern = "^[a-fA-F0-9]{40}$"
+        guard string.range(of: pattern, options: .regularExpression) != nil else { return nil }
+        
+        if string.lowercased() == string || string.uppercased() == string {
+            hasChecksum = false
+        } else {
+            /*
+            let hashBytes = [UInt8](keccak256(string.lowercased()))
+            print("string size: \(string.count)")
+            print("hash bytes size: \(hashBytes.count)")
+            hashBytes.forEach { print("\($0) ", terminator: "") }
+            for idx in 0..<40 {
+               let index = string.index(string.startIndex, offsetBy: idx)
+                let char = String(string[index])
+                if (hashBytes[idx] > 7 && char.uppercased() != char)
+                    || (hashBytes[idx] <= 7 && char.lowercased() != char) {
+                    return nil
+                }
+            }
+ */
+            hasChecksum = true
+        }
+        self.string = "0x" + string
+    }
+}
---
+++ Teambrella/Model/Entities/WithdrawTx.swift
@@ -0,0 +1,48 @@
+//
+/* Copyright(C) 2017 Teambrella, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License(version 3) as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see<http://www.gnu.org/licenses/>.
+ */
+
+import Foundation
+import SwiftyJSON
+
+struct WithdrawTx {
+    let id: String
+    let lastUpdated: Int64
+    
+    let withdrawalID: Int
+    let withdrawalDate: Date?
+    let isNew: Bool
+    let amount: Double
+    
+    let serverTxState: Int
+    
+    init?(json: JSON) {
+        guard let id = json["Id"].string,
+            let lastUpdated = json["LastUpdated"].int64,
+            let withdrawalID = json["WithdrawalId"].int,
+            let serverTxState = json["ServerState"].int else { return nil }
+        
+        self.id = id
+        self.lastUpdated = lastUpdated
+        self.withdrawalID = withdrawalID
+        self.serverTxState = serverTxState
+        
+        self.withdrawalDate = Formatter.teambrella.date(from: json["WithdrawalDate"].stringValue)
+        self.isNew = json["IsNew"].boolValue
+        
+        let to = json["To"].arrayValue
+        self.amount = to.first?["Amount"].doubleValue ?? 0
+    }
+}
