8c75e226c02f92f817f5a959015f67e795ef359a
Thorax <thorax@me.com>
2018-05-23 21:12:53 +0300

    update builder script

--- builder.sh
+++ builder.sh
@@ -1,6 +1,39 @@
 #!/bin/bash
 
-sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
+#sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
+
+echo "Checking whether all necessary programms are installed..."
+if gem list pod -i > /dev/null; then
+  echo "Cocoapods is installed"
+else
+  echo "Installing Cocoapods"
+  gem install cocoapods
+fi
+
+command -v brew >/dev/null 2>&1 || { echo >&2 "Installing Homebrew"; \
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"; }
+
+if brew ls --versions carthage > /dev/null; then
+  echo "Carthage is installed"
+else
+  echo "Installing Carthage"
+  brew install carthage
+fi
+
+echo "Retreiving carthage dependencies. This may take a while..."
 carthage update --platform iOS
+echo "Retreiving Cocoapods dependencies. This should be quite fast..."
 pod update
+
+resources="Resources"
+if test "$(ls "$resources")"; then
+     echo "$resources already populated"
+else
+    echo "$resources folder is Empty. Cloning private repo..."
+    git submodule init
+    git submodule update
+    echo "Finished cloning private resources."
+fi
+
+echo "Teambrella is ready to use"
 say Сборка проэкта Тимбрэлла, завершена
