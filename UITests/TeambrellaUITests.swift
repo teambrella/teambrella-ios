21c051819277086aae8f1eac537eb394202815fa
Thorax <thorax@me.com>
2018-06-26 12:14:00 +0300

    add Snapshot (automate screenshots)

--- UITests/TeambrellaUITests.swift
+++ UITests/TeambrellaUITests.swift
@@ -17,28 +17,64 @@
 import XCTest
 
 class TeambrellaUITests: XCTestCase {
-        
+    var app: XCUIApplication!
+
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        self.app = app
+        app.launch()
+
+        continueAfterFailure = true
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
+
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+    }
+
+    func testScreenshots() {
+        // tap "Try Demo"
+        app.buttons["tryDemo"].tap()
+
+        // tap "Home"
+        let tabBarsQuery = app.tabBars
+        tabBarsQuery.buttons.element(boundBy: 0).tap()
+        sleep(1)
+
+        snapshot("0Home")
+
+        // tap "Team"
+        tabBarsQuery.buttons.element(boundBy: 1).tap()
+
+        let collectionViewsQuery3 = app.collectionViews
+        let collectionViewsQuery = collectionViewsQuery3
+
+        // tap Claims
+        collectionViewsQuery.staticTexts.element(boundBy: 2).tap()
+
+        let collectionViewsQuery2 = app.scrollViews.otherElements.collectionViews
+        collectionViewsQuery2.children(matching: .cell).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.tap()
+
+        sleep(1)
+        collectionViewsQuery.sliders.element(boundBy: 0).adjust(toNormalizedSliderPosition: 0.66)
+
+        snapshot("1Claim")
+
+        // tap Chat
+        collectionViewsQuery.cells["imageGalleryCell"].children(matching: .other).element.children(matching: .other).element(boundBy: 1).tap()
+        app.collectionViews["UniversalChatCollectionView"].swipeUp()
+        snapshot("2Chat")
     }
     
 }
