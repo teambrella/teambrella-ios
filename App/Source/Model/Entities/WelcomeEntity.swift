059d72ce2d04db0a79ba8b8905680c29bf7cf791
Thorax <thorax@me.com>
2018-10-03 17:51:47 +0300

    add welcomeEntity

---
+++ App/Source/Model/Entities/WelcomeEntity.swift
@@ -0,0 +1,39 @@
+//
+/* Copyright(C) 2016-2018 Teambrella, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License(version 3) as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+
+import Foundation
+
+struct WelcomeEntity: Decodable {
+    let teamID: Int
+    let teamName: String
+    let teamLogo: Photo
+    let location: String
+    let nameFrom: Name?
+    let nameTo: Name?
+    let title: String
+    let text: String
+    
+    enum CodingKeys: String, CodingKey {
+        case teamID = "TeamId"
+        case teamName = "TeamName"
+        case teamLogo = "TeamLogo"
+        case nameFrom = "NameFrom"
+        case location = "Location"
+        case nameTo = "NameTo"
+        case title = "WelcomeTitle"
+        case text = "WelcomeText"
+    }
+}
