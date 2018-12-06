43f9db429bedf1882358739bb348c5a61ac06735
Thorax <thorax@me.com>
2018-12-06 21:20:05 +0300

    fix color for delete image button in chat

--- App/Source/Scenes/Team/Chat/ChatImageCell.swift
+++ App/Source/Scenes/Team/Chat/ChatImageCell.swift
@@ -89,7 +89,9 @@ class ChatImageCell: UICollectionViewCell, ChatUserDataCell {
         let button = UIButton()
         button.frame = CGRect(x: 0, y: 0, width: size, height: size)
         button.backgroundColor = UIColor.black.withAlphaComponent(0.2)
-        button.setImage(#imageLiteral(resourceName: "crossIcon"), for: .normal)
+        let image = #imageLiteral(resourceName: "crossIcon").withRenderingMode(.alwaysTemplate)
+        button.tintColor = .white
+        button.setImage(image, for: .normal)
         button.addTarget(self, action: #selector(tapDelete), for: .touchUpInside)
         button.layer.cornerRadius = 20
         button.layer.masksToBounds = true
