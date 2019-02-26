2d9db2bc77551bda59576f07ce0dd628a923ff78
paperno <alex.paperno@gmail.com>
2019-02-26 17:06:36 +0300

    Risks & Claims voting stats

--- App/Source/VotingStats/ClaimsStatsCell.swift
+++ App/Source/VotingStats/ClaimsStatsCell.swift
@@ -16,35 +16,49 @@
 
 import UIKit
 
-class OthersVotedCell: UICollectionViewCell {
-    @IBOutlet var avatarView: RoundImageView!
-    @IBOutlet var nameLabel: Label!
-    @IBOutlet var subtitleLabel: Label!
-    @IBOutlet var subtitleValueLabel: Label!
-    @IBOutlet var valueLabel: Label!
-    @IBOutlet var separatorView: UIView!
-    @IBOutlet var separatorLeadingInset: NSLayoutConstraint!
+class ClaimsStatsCell: UICollectionViewCell {
     
+    @IBOutlet var avatarView: UIImageView!
+    @IBOutlet var titleLabel: Label!
+    @IBOutlet var ownerAvatarView: RoundImageView!
+    @IBOutlet var ownerNameLabel: Label!
+    @IBOutlet var amountLabel: Label!
+    @IBOutlet var cellSeparator: UIView!
+    @IBOutlet var statusLabel: Label!
+    @IBOutlet var badge: Label!
+
     override func awakeFromNib() {
         super.awakeFromNib()
-        
-        subtitleLabel.font = UIFont.teambrella(size: 12)
-        subtitleLabel.textColor = UIColor.lightTextColor
-        
-        subtitleValueLabel.font = UIFont.teambrellaBold(size: 12)
-        subtitleValueLabel.textColor = UIColor.darkTextColor
-        
-        valueLabel.font = UIFont.teambrellaBold(size: 20)
-        valueLabel.textColor = UIColor.darkTextColor
-        
-        separatorView.backgroundColor = UIColor.separatorColor
     }
     
-    func update(with model: OthersVotedCellModel) {
-        avatarView.show(model.avatar)
-        nameLabel.text = model.name
-        subtitleLabel.text = model.subtitle
-        subtitleValueLabel.text = model.subtitleValue
-        valueLabel.text = model.value
+    func update(with claim: ClaimEntity) {
+        ownerAvatarView.show(claim.avatar)
+        titleLabel.text = claim.model
+        ownerNameLabel.text = claim.name.entire.uppercased()
+        avatarView.show(claim.smallPhoto)
+        
+        amountLabel.text = claim.myVote?.stringRounded ?? ""
+        amountLabel.font = isSmallIPhone ? UIFont.teambrellaBold(size: 16) : UIFont.teambrellaBold(size: 20)
+        amountLabel.textColor = .blueyGray
+
+        let teamVote = Int(claim.reimbursement.value*100 + 0.5)
+        let myVote = Int((claim.myVote?.value ?? 0)*100 + 0.5)
+        let diff = myVote - teamVote
+        if diff > 0 {
+            amountLabel.textColor = .darkSkyBlue
+        } else if diff < 0 {
+            amountLabel.textColor = .perrywinkle
+        }
+
+        statusLabel.text = claim.proxyName != nil ? "Team.TeammateCell.byProxy".localized.uppercased() : ""
+        statusLabel.alpha = 0.5
+
+        badge.text = String(format: "%d%%", teamVote)
+        badge.layer.masksToBounds = true
+        badge.layer.borderWidth = 1.5
+        let badgeFont: UIFont = UIFont.teambrella(size: 10)
+        let badgeTextColor: UIColor = .white
+        badge.layer.borderColor = badgeTextColor.cgColor
+        badge.font = badgeFont
     }
  }
